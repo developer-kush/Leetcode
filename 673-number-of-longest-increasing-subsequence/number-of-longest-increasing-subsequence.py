9deccd29b516641bc448d9484cc9dd2302901b29
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-07-21 15:45:36 +0530

    Time: 1081 ms (82.10%) | Memory: 16.5 MB (94.46%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
@@ -3,20 +3,19 @@ class Solution:
         n = len(nums)
         if n <= 1: return n
 
-        lengths = [1] * n
-        counts = [1] * n
+        lengths, cnts = [1]*n, [1]*n
 
         for i in range(1, n):
             for j in range(i):
                 if nums[j] < nums[i]:
                     if lengths[j] >= lengths[i]:
                         lengths[i] = lengths[j] + 1
-                        counts[i] = counts[j]
-                    elif lengths[j] == lengths[i]-1:
-                        counts[i] += counts[j]
+                        cnts[i] = cnts[j]
+                    elif lengths[j]+1 == lengths[i]:
+                        cnts[i] += cnts[j]
 
         max_len = max(lengths)
         res = 0
         for idx, i in enumerate(lengths):
-            if i == max_len: res += counts[idx]
+            if i == max_len: res += cnts[idx]
         return res
