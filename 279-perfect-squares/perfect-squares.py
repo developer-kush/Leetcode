53bebd8f788c3d4111d254b2b4c8ab1f169958eb
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-07-23 22:26:54 +0530

    Time: 3917 ms (60.25%) | Memory: 16.4 MB (80.60%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -1,16 +1,27 @@
+inf = float('inf')
+
 class Solution:
     def numSquares(self, n: int) -> int:
+
+        memo = [inf]*(n+1)
+        memo[0] = 0
+
+        for i in range(1, n+1):
+            for j in range(int(sqrt(i))+1):
+                memo[i] = min(memo[i], memo[i-j*j]+1)
+
+        return memo[n]    
         
-        @cache
-        def rec(n):
-            if n < 0: return float('inf')
-            if n == 0: return 0
 
-            res = float('inf')
+        # @cache
+        # def rec(n): # MINIMUM PERFECT SQUARES TO MAKE N
+        #     if n < 0: return float('inf')
+        #     if n == 0: return 0
 
-            for i in range(int(sqrt(n)),0,-1):
-                res = min(res, rec(n-i*i)+1)
+        #     res = float('inf')
+        #     for i in range(int(sqrt(n)),0,-1):
+        #         res = min(res, rec(n-i*i)+1)
 
-            return res
+        #     return res
 
-        return rec(n)
+        # return rec(n)
