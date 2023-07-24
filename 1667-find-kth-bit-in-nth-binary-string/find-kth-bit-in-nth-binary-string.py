b7212d7987f81a062f08b0cd63810fe2bbd99ce0
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-07-25 00:04:30 +0530

    Time: 56 ms (74.07%) | Memory: 16.2 MB (91.53%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -1,13 +1,10 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
 
-        # 0111001
-        # 0111001  1  011 0 001
-
         ln = (1<<n) - 1
 
         def rec(k, inv, ln):
-            
+
             if ln == 1:
                 if inv&1: return '1'
                 return '0'
@@ -17,7 +14,7 @@ class Solution:
                 if inv&1: return'0'
                 return '1'
             
-            if k > nln: return rec(nln+nln-k, inv+1, nln)
+            if k > nln: return rec((nln<<1)-k, inv+1, nln)
             
             return rec(k, inv, nln)
 
