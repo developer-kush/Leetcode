8ca29a7049a9324c49ed699eeec35dcb219b6272
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-07-24 21:48:52 +0530

    Time: 36 ms (94.12%) | Memory: 16.3 MB (63.03%) - LeetSync

--- 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
+++ 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
@@ -2,12 +2,11 @@ class Solution:
     def monkeyMove(self, n: int) -> int:
         MOD = 10**9 + 7
 
-        @cache
         def fastPow(x, n):
             if x <= 1: return x
             if n == 0: return 1
             res = fastPow(x, n//2)
             if n&1 : return (res*res*x) % MOD
-            return res*res % MOD
+            return (res*res) % MOD
 
         return (fastPow(2, n)-2)%MOD
