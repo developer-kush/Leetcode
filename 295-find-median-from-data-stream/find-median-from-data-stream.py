347833f5eb0e9703eeaf36067f3b63bec1328c33
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-07-25 22:42:44 +0530

    Time: 472 ms (93.41%) | Memory: 38.7 MB (18.14%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -4,19 +4,30 @@ from bisect import insort
 class MedianFinder:
 
     def __init__(self):
-        self.hp = []
+        self.lo = []
+        self.hi = []
 
     def addNum(self, num: int) -> None:
-        insort(self.hp, num)
+        if self.hi and num >= self.hi[0]: heappush(self.hi, num)
+        else: heappush(self.lo, -num)
+
+        if len(self.hi)-len(self.lo) >= 2: heappush(self.lo, -heappop(self.hi) )
+        if len(self.lo)-len(self.hi) >= 2: heappush(self.hi, -heappop(self.lo) )
 
     def findMedian(self) -> float:
-        ln = len(self.hp)
-        median = self.hp[ln//2]
-        if not ln&1:
-            median = (median + self.hp[ln//2 - 1])/2
-        return median
-        
-# 1 2 3 4 
+        if len(self.lo)> len(self.hi): return -self.lo[0]
+        elif len(self.hi) > len(self.lo): return self.hi[0]
+        else: return (self.hi[0]-self.lo[0])/2
+
+# 7 - ___ -> 7
+# 3 7 - ___ -> 3 - 7 
+# 3 - 7 13 -> 7
+
+
+
+# 1. if ln(h1)-ln(hp2)>=2 move 1 element to hp2 ( balance both )
+# 2. if ln(hp1) - ln(hp2) == 1 or 0
+
 
 
 # Your MedianFinder object will be instantiated and called as such:
