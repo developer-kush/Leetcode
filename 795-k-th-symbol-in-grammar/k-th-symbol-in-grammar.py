0c45d1c68907dc247f1d21dfae0423446f2c3f79
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-10-25 06:49:58 +0530

    Time: 36 ms (73.58%) | Memory: 16.2 MB (79.64%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -1,8 +1,8 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        
-        curr = 0
-        for move in (bin(k-1)[2:]).rjust(n, '0'):
-            if move == '1': curr = 1 - curr
-
-        return curr
+        k-=1
+        tot = 0
+        while k:
+            if k&1: tot = 1 - tot
+            k >>= 1
+        return tot
