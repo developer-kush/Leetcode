14be6709a73869d92c770bf0a6b55a8906a61977
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-10-26 13:51:09 +0530

    Time: 898 ms (28.72%) | Memory: 35.5 MB (27.66%) - LeetSync

--- 814-smallest-rotation-with-highest-score/smallest-rotation-with-highest-score.py
+++ 814-smallest-rotation-with-highest-score/smallest-rotation-with-highest-score.py
@@ -5,24 +5,13 @@ class Solution:
 
         # Marking rotatable ranges
         for idx, num in enumerate(nums):
-            l, r = idx, (idx-num+n)%n
-            # print(num, ":", f"{l} , {r}".rjust(7,' '), ":", end=" ")
+            l, r = idx, (idx-num)%n
             rangeMap[l] += 1
             rangeMap[r+1] -= 1
-
-        # print("vals :",*[rangeMap[i] for i in range(n)])
 
         # Finding the best rotational value
         tot = midx = mxval = 0
         for i in range(len(nums)):
             tot += rangeMap[i]
-            # print(tot, end=" ")
             if tot > mxval: midx, mxval = i, tot
         return midx
-'''
----
---- -
-   --
------
- -
-'''
