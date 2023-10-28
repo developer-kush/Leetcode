d63a58956d0d034f064c8996d3f7b421a193a806
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-10-28 07:40:17 +0530

    Time: 103 ms (87.08%) | Memory: 16.1 MB (95.14%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.py
+++ 1332-count-vowels-permutation/count-vowels-permutation.py
@@ -3,22 +3,14 @@ class Solution:
         MOD = int(1e9 + 7)
         
         dp=[1,1,1,1,1]
-        chars={
-            0:[1,2,4],
-            1:[0,2],
-            2:[1,3],
-            3:[2],
-            4:[2,3]
-        }
         
         for i in range(2,n+1):
-            new = [
+            dp = [
                 (dp[1]+dp[2]+dp[4])%MOD,
                 (dp[0]+dp[2])%MOD,
                 (dp[1]+dp[3])%MOD,
                 dp[2],
                 (dp[2]+dp[3])%MOD
             ]
-            dp=new
         
         return sum(dp)%MOD
