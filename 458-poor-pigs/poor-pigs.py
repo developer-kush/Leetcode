02967704800e490fef2880fff4f52954d6159d3b
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-10-29 08:09:08 +0530

    Time: 36 ms (68.80%) | Memory: 16.4 MB (13.60%) - LeetSync

--- 458-poor-pigs/poor-pigs.py
+++ 458-poor-pigs/poor-pigs.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
-        return int(math.ceil(math.log2(buckets)/math.log2((minutesToTest//minutesToDie)+1)))
+        return int(ceil(log2(buckets)/log2((minutesToTest//minutesToDie)+1)))
