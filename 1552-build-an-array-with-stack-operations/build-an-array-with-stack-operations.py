e0bb2a0976fa3d2694a602caed4fdfeaa66a51b0
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-11-03 20:19:13 +0530

    Time: 39 ms (65.86%) | Memory: 16.1 MB (76.57%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -2,6 +2,7 @@ class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         res = ["Push", "Pop"]*(target[0]-1)
         for idx in range(1, len(target)):
-            res.extend(["Push"]+["Push", "Pop"]*(target[idx]-target[idx-1]-1))
+            res.append("Push")
+            res.extend(["Push", "Pop"]*(target[idx]-target[idx-1]-1))
         res.append("Push")
         return res
