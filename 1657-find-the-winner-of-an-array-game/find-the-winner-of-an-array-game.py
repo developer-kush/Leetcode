3034368f5db9e0ca8094ddd31434aad9bc806f71
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-11-06 04:16:23 +0530

    Time: 504 ms (95.70%) | Memory: 30 MB (51.61%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
@@ -1,22 +1,11 @@
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
         if k >= len(arr): return max(arr)
-        mx = max(arr)
-        q = deque(arr)
-
-        while q[0] != mx:
-            cnt = 0
-            a, b = q.popleft(), q.popleft()
-            while cnt!=k and a > b:
-                cnt += 1
-                q.appendleft(a)
-                q.append(b)
-                a, b = q.popleft(), q.popleft()
-    
-            if cnt == k: return a
-
-            if b > a: 
-                q.appendleft(a)
-                q.appendleft(b)
-        
-        return mx
+        cnt, curr = 0, arr[0]
+        for i in range(1, len(arr)):
+            if arr[i] < curr: cnt += 1
+            else:
+                cnt = 1
+                curr = arr[i]
+            if cnt == k: return curr
+        return curr
