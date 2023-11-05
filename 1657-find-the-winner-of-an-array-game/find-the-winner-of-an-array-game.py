ada6c3a6d43839dd9ee4c5843f2455bc264a50aa
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-11-06 04:17:28 +0530

    Time: 514 ms (86.02%) | Memory: 30 MB (63.44%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
@@ -3,7 +3,10 @@ class Solution:
         if k >= len(arr): return max(arr)
         cnt, curr = 0, arr[0]
         for i in range(1, len(arr)):
-            if arr[i] < curr: cnt += 1
-            else: cnt, curr = 1, arr[i]
+            if arr[i] < curr: 
+                cnt += 1
+            else:
+                cnt = 1
+                curr = arr[i]
             if cnt == k: return curr
         return curr
