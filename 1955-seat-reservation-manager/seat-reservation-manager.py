3b5afb5ca442565e72d26e11e25d4ca53cb60c4d
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-11-06 08:39:55 +0530

    Time: 375 ms (96.78%) | Memory: 42.4 MB (98.79%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.py
+++ 1955-seat-reservation-manager/seat-reservation-manager.py
@@ -1,10 +1,15 @@
 class SeatManager:
 
     def __init__(self, n: int):
-        self.available = list(range(1,n+1))
+        self.marker = 1
+        self.available = [1]
 
     def reserve(self) -> int:
-        return heappop(self.available)
+        res = heappop(self.available)
+        if res == self.marker:
+            self.marker += 1
+            heappush(self.available, self.marker)
+        return res
 
     def unreserve(self, seatNumber: int) -> None:
         heappush(self.available, seatNumber)
