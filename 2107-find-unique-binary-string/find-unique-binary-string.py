47a6c9e4d00421a5bccb0ad3060d644437656a91
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-11-16 08:55:30 +0530

    Time: 31 ms (97.71%) | Memory: 16.3 MB (66.27%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,6 +1,5 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        nums = set(nums)
         for i in range(len(nums)+1):
             if (curr:=bin(i)[2:].rjust(len(nums), '0')) not in nums:
                 return curr
