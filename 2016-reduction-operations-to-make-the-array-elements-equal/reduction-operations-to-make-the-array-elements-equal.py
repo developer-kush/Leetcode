55d06f606b58122b14ce4b276fcad494c13feed9
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-11-19 07:55:25 +0530

    Time: 830 ms (70.10%) | Memory: 27.3 MB (35.57%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
@@ -2,5 +2,4 @@ class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
         ctr = Counter(nums)
         keys = sorted(ctr)
-
         return sum(idx*ctr[key] for idx, key in enumerate(keys))
