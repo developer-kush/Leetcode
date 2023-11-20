0ee99f60636d8fcfd5572ccedb47b9274fdd514f
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-11-20 08:43:22 +0530

    Time: 1189 ms (10.41%) | Memory: 59 MB (5.12%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
@@ -1,10 +1,7 @@
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
         travel.append(0)
-        res=0
-        currdist=0
-        gfarthest=mfarthest=pfarthest=0
-        g=m=p=0
+        res=currdist=gfarthest=mfarthest=pfarthest=g=m=p=0
         for i in range(len(garbage)):
             garbage[i]=collections.Counter(garbage[i])
             if 'G' in garbage[i]: g,gfarthest=g+garbage[i]['G'],currdist
