f71b5b864149cb638bf93599a376337e01426b3a
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-12-06 09:57:20 +0530

    Time: 39 ms (60.62%) | Memory: 16.3 MB (7.77%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -1,4 +1,4 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
         g, r = divmod(n, 7)
-        return 7*((g<<2) + ((g*(g-1))>>1)) + ((r*(r+1 + (g<<1)))>>1) 
+        return 7*((g<<2) + ((g*(g-1))>>1)) + ((r*(r+1))>>1) + g*r
