f0637c40ef3062ff70545e3aede25006667afc55
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2023-12-17 17:08:06 +0530

    Time: 763 ms (58.33%) | Memory: 48 MB (55.36%) - LeetSync

---
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.py
@@ -0,0 +1,27 @@
+class FoodRatings:
+
+    def __init__(self, foods, cuisines, ratings):
+        self.cuisine_to_heap = defaultdict(list)
+        self.food_to_cuisine = {}
+        self.food_to_rating = defaultdict(int)
+        for i in range(len(foods)):
+            self.food_to_cuisine[foods[i]] = cuisines[i]
+            heapq.heappush(self.cuisine_to_heap[cuisines[i]], (-ratings[i], foods[i]))
+            self.food_to_rating[foods[i]] = -ratings[i]
+        
+
+    def changeRating(self, food, newRating):
+        cuisine = self.food_to_cuisine[food]
+        heapq.heappush(self.cuisine_to_heap[cuisine], (-newRating, food))
+        self.food_to_rating[food] = -newRating
+
+    def highestRated(self, cuisine):
+        smallest_lexico = None
+        while len(self.cuisine_to_heap[cuisine]) > 0:
+            curr = self.cuisine_to_heap[cuisine][0]
+            if curr[0] != self.food_to_rating[curr[1]]:
+                heapq.heappop(self.cuisine_to_heap[cuisine])
+                continue
+            smallest_lexico = curr[1]
+            break
+        return smallest_lexico
