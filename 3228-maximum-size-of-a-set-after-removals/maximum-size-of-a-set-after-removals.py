09024f17e5f7dab0e047855d01e41eafc124f40c
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-01-07 20:27:00 +0530

    Time: 506 ms (33.33%) | Memory: 27.3 MB (100.00%) - LeetSync

--- 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.py
+++ 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.py
@@ -1,10 +1,7 @@
 class Solution:
     def maximumSetSize(self, nums1: List[int], nums2: List[int]) -> int:
-        N = len(nums1)
-        nums1, nums2 = set(nums1), set(nums2)
-        both = nums1 & nums2
-        one = nums1 ^ both
-        two = nums2 ^ both
-        from_one = min(N//2, len(one))
-        from_two = min(N//2, len(two))
-        return min(from_one + from_two + len(both), N)
+        n = len(nums1)
+        s1, s2 = set(nums1), set(nums2)
+        a, b, both = len(s1), len(s2), len(s1&s2)
+        
+        return min(n, min(n>>1, a-both)+min(n>>1, b-both)+both)
