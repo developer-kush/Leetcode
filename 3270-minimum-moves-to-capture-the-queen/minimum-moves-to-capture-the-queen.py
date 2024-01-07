d9fe815a3d2ea73dd9a8f94ec79f6ea75c9d1fba
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-01-07 13:58:23 +0530

    Time: 31 ms (100.00%) | Memory: 17.3 MB (50.00%) - LeetSync

--- 3270-minimum-moves-to-capture-the-queen/minimum-moves-to-capture-the-queen.py
+++ 3270-minimum-moves-to-capture-the-queen/minimum-moves-to-capture-the-queen.py
@@ -1,17 +1,7 @@
 class Solution:
     def minMovesToCaptureTheQueen(self, a: int, b: int, c: int, d: int, e: int, f: int) -> int:
-        # grid = [['.']*8 for _ in range(8)]
-        # grid[a-1][b-1] = 'R'
-        # grid[c-1][d-1] = 'B'
-        # grid[e-1][f-1] = 'Q'
 
-
-        # for row in grid: print(*row)
-
-        if a == e:
-            if c == e :
-                if sorted([b, d, f])[1]!=d: return 1
-            else: return 1
+        if a == e and (c!=e or sorted([b, d, f])[1]!=d): return 1
         if b == f: 
             if d == f :
                 if sorted([a, c, e])[1]!=c: return 1
