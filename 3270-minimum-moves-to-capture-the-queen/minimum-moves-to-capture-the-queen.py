2fbb9afe6fb4579a5580beec58ca80920fd77f76
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-01-07 14:00:20 +0530

    Time: 30 ms (100.00%) | Memory: 17.4 MB (50.00%) - LeetSync

--- 3270-minimum-moves-to-capture-the-queen/minimum-moves-to-capture-the-queen.py
+++ 3270-minimum-moves-to-capture-the-queen/minimum-moves-to-capture-the-queen.py
@@ -1,14 +1,8 @@
 class Solution:
     def minMovesToCaptureTheQueen(self, a: int, b: int, c: int, d: int, e: int, f: int) -> int:
 
-        if a == e and (c!=e or sorted([b, d, f])[1]!=d): return 1
-        if b == f: 
-            if d == f :
-                if sorted([a, c, e])[1]!=c: return 1
-            else: return 1
-
+        if a == e and (c != e or sorted([b, d, f])[1]!=d): return 1
+        if b == f and (d != f or sorted([a, c, e])[1]!=c): return 1
         if c + d == e + f and (a+b!=e+f or sorted([c, a, e])[1]!=a): return 1
-
         if c - d == e - f and (a-b!=e-f or sorted([c, a, e])[1]!=a): return 1
-
         return 2
