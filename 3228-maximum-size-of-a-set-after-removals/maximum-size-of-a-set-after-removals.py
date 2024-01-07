726e30fe9eae10538e24016700accbaaa94e2973
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-01-07 20:26:07 +0530

    Time: 493 ms (33.33%) | Memory: 27.3 MB (100.00%) - LeetSync

--- 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.py
+++ 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.py
@@ -3,8 +3,5 @@ class Solution:
         n = len(nums1)
         s1, s2 = set(nums1), set(nums2)
         a, b, both = len(s1), len(s2), len(s1&s2)
-        onlya, onlyb = a-both, b-both
-
-        tot = min(n>>1, onlya)+min(n>>1, onlyb)+both
-
-        return min(n, tot)
+        
+        return min(n, min(n>>1, a-both)+min(n>>1, b-both)+both)
