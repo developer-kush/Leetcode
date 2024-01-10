2c8575d8f680773f5fa667c1a2b19fe49fe05d54
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-01-10 08:56:59 +0530

    Time: 456 ms (78.03%) | Memory: 83.2 MB (50.84%) - LeetSync

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        
+        tree = defaultdict(list)
+        
+        def rec(root):
+            if root is None: return
+            if root.left:
+                tree[root.val].append(root.left.val)
+                tree[root.left.val].append(root.val)
+            if root.right:
+                tree[root.val].append(root.right.val)
+                tree[root.right.val].append(root.val)
+            rec(root.left)
+            rec(root.right)
+
+        rec(root)
+
+        visited = set()
+        def rec(root, dist = 0):
+            res = dist
+            visited.add(root)
+            for ne in tree[root]:
+                if ne not in visited: 
+                    visited.add(ne)
+                    res = max(res, rec(ne, dist+1))
+            return res
+
+        return rec(start)
