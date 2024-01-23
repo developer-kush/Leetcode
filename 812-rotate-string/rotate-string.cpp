c25558eba916fea711c3db68ca0275c963ce56d0
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-01-23 11:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (20.35%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,14 @@
+# include <string.h>
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.size()!=goal.size()){
+            return false;
+        }
+        
+        s = s+s;
+        
+        return s.find(goal) != string::npos ;
+    }
+};
