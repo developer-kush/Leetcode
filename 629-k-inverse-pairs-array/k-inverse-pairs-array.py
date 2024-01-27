cf0c277c965b98fcb0704081058adc5d9a94d58e
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-01-27 09:45:13 +0530

    Time: 157 ms (97.86%) | Memory: 16.7 MB (87.14%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.py
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.py
@@ -1,11 +1,11 @@
 class Solution:
     def kInversePairs(self, n: int, k: int) -> int:
-        dp,md=[1]+[0]*k,10**9+7
+        dp,MOD=[1]+[0]*k,10**9+7
         for i in range(n):
             tmp,curr=[],0
             for j in range(k+1):
                 curr+=dp[j]
                 if j-i>=1: curr-=dp[j-i-1]
-                tmp.append(curr%md)
+                tmp.append(curr%MOD)
             dp=tmp
         return dp[-1]
