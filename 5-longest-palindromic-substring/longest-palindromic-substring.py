32e1f1795a9ceaa3995388db2f0f0a3ea0ab1c7a
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-02-02 18:40:31 +0530

    Time: 293 ms (80.97%) | Memory: 16.6 MB (75.80%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,20 +1,15 @@
 def expand(s,i,j):
-    res=[i,j]
-    while 0<=i<=j<len(s) and s[i]==s[j]:
-        res=[max(0,i),min(len(s)-1,j)]
-        i-=1
+    if s[i]!=s[j]: return ""
+    while i>=0 and j<len(s) and s[i] == s[j]:
+        i-=1 
         j+=1
-    return res
-    
+    return s[i+1:j]
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n=len(s)
-        res=[0,0]
+        res=''
         for i in range(n):
-            s1=expand(s,i,i)
-            if i<n-1 and s[i]==s[i+1]:
-                s2=expand(s,i,i+1)
-            else: s2=[0,0]
-            res=max(res,s1,s2,key=lambda x: x[1]-x[0]+1)
-        return s[res[0]:res[1]+1]
+            res = max(res, expand(s,i,i), key=len)
+            if i<n-1: res = max(res, expand(s,i,i+1), key=len)
+        return res
