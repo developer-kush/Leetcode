1b429fe28ade0c8b5b41884e99d576cbf3e3494b
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-02-09 19:44:06 +0530

    Time: 322 ms (31.16%) | Memory: 16.9 MB (76.96%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -11,7 +11,8 @@ class Solution:
             for i in range(1, int(sqrt(val))+1):
                 if val%i==0: 
                     if dp.get(i, 0) > res: res, prev[val] = dp[i], i
-                    if dp.get(val//i, 0) > res: res, prev[val] = dp[val//i], val//i
+                    i = val//i
+                    if dp.get(i, 0) > res: res, prev[val] = dp[i], i
             dp[val] = res + 1
 
         maxval = max(dp.values())
