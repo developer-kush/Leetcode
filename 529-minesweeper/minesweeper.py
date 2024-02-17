653edf637bb58681c4e09dcc87b73cf165f29f2a
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-02-17 12:05:54 +0530

    Time: 157 ms (63.44%) | Memory: 16.9 MB (89.10%) - LeetSync

---
+++ 529-minesweeper/minesweeper.py
@@ -0,0 +1,36 @@
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        n, m = len(board), len(board[0])
+
+        # def printboard(board):
+        #     for row in board: print(*['.' if curr == 'B' else curr for curr in row])
+        # printboard(board)
+        # print("---")
+
+        def mineVal(i, j):
+            return sum(
+                (0<=r<n and 0<=c<m and board[r][c]=='M') 
+                for r, c in [
+                    [i-1,j-1],[i-1,j],[i-1,j+1], [i,j-1], 
+                    [i,j+1], [i+1,j-1], [i+1,j], [i+1,j+1]
+                ]
+            )
+
+        if board[click[0]][click[1]] == 'M':
+            board[click[0]][click[1]] = 'X'
+        else: 
+            q = deque([click])
+            while q:
+                r, c = q.popleft()
+                if board[r][c] != 'E': continue
+                cnt = mineVal(r, c)
+                if cnt: 
+                    board[r][c] = str(cnt); continue
+                board[r][c] = 'B'
+                for x, y in ((r-1, c), (r-1, c-1), (r-1, c+1), (r+1, c), (r+1, c-1), (r+1, c+1), (r, c-1), (r, c+1)):
+                    if not (0<=x<n and 0<=y<m and board[x][y]=='E'): continue
+                    q.append((x,y))
+
+        # printboard(board)
+
+        return board
