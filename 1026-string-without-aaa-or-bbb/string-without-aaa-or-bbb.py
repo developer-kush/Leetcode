09b40bf9830178f0b5c8dc158c8a48d3310a8039
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-02-20 02:56:21 +0530

    Time: 34 ms (70.90%) | Memory: 16.6 MB (76.72%) - LeetSync

--- 1026-string-without-aaa-or-bbb/string-without-aaa-or-bbb.py
+++ 1026-string-without-aaa-or-bbb/string-without-aaa-or-bbb.py
@@ -2,7 +2,8 @@ class Solution:
     def strWithout3a3b(self, a: int, b: int) -> str:
         if a > b:
             rems = a-b
-            return ('aab'*rems + 'ab'*(b-rems))[:a+b]
-        else:
-            rems = b-a
-            return ('bba'*rems + 'ab'*(a-rems))[:a+b]
+            if rems*3 > a+b: return ("aab")*((a+b)//3) + "aab"[:(a+b)%3]
+            return ('aab'*rems + 'ab'*(b-rems))
+        rems = b-a
+        if rems*3 > a+b: return ("bba")*((a+b)//3) + "bba"[:(a+b)%3]
+        return ('bba'*rems + 'ba'*(a-rems))
