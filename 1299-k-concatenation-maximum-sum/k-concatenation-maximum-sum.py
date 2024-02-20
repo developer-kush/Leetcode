9e090961a64b81e36d2d5df71f2c0d864bf81753
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-02-21 00:10:33 +0530

    Time: 404 ms (27.46%) | Memory: 31.1 MB (72.54%) - LeetSync

--- 1299-k-concatenation-maximum-sum/k-concatenation-maximum-sum.py
+++ 1299-k-concatenation-maximum-sum/k-concatenation-maximum-sum.py
@@ -1,6 +1,8 @@
 class Solution:
     def kConcatenationMaxSum(self, arr: List[int], k: int) -> int:
 
+        MOD = int(1e9 + 7)
+
         def kadaneModified(arr, res=0, curr=0, currmin=0):
             for num in arr:
                 curr+=num
@@ -8,15 +10,15 @@ class Solution:
                 res = max(res, curr-currmin)
             return (res, curr, currmin)
 
-        if k == 1: return kadaneModified(arr)[0]
+        if k == 1: return kadaneModified(arr)[0] % MOD
 
         tot = sum(arr) * (k-2)
+        if tot <= 0: return kadaneModified(arr, *kadaneModified(arr))[0] % MOD
+
         res, curr, currmin = kadaneModified(arr)
-        possible = kadaneModified(arr, res, curr, currmin)[0]
+        # possible = kadaneModified(arr, res, curr, currmin)[0]
         curr += tot
         currmin = min(currmin, curr)
         res = max(res, curr-currmin)
-
         res, curr, currmin = kadaneModified(arr, res, curr, currmin)
-
-        return max(possible, res) % int(1e9 + 7)
+        return res % MOD
