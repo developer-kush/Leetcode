5c1d11c18ff1ddcba4ca2d38f272b3989610fd28
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-02-21 00:28:28 +0530

    Time: 1915 ms (25.89%) | Memory: 123.9 MB (17.65%) - LeetSync

---
+++ 1281-can-make-palindrome-from-substring/can-make-palindrome-from-substring.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canMakePaliQueries(self, s: str, queries: List[List[int]]) -> List[bool]:
+
+        qstarts = set(query[0]-1 for query in queries)
+        res = [0]*len(queries)
+
+        reqmap = defaultdict(list)
+        for idx, (st, e, k) in enumerate(queries): reqmap[e].append((st, k, idx))
+
+        counts = {}
+        ctr = Counter()
+        if -1 in qstarts: counts[-1] = dict(ctr)
+
+        for i in range(len(s)):
+            ctr[s[i]] += 1
+            if i in qstarts: counts[i] = dict(ctr)
+            
+            for st, k, idx in reqmap[i]:
+                cs = counts[st-1]
+                diffs = sum((ctr[chr(ch)]-cs.get(chr(ch), 0))&1 for ch in range(97, 97+26))
+                res[idx] = (diffs//2) <= k
+
+        return res
