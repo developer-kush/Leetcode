d4be5b38c42a5dac28a356a47b25f14d4e0f8645
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-02-21 01:16:37 +0530

    Time: 37 ms (53.97%) | Memory: 16.6 MB (58.20%) - LeetSync

--- 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.py
+++ 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.py
@@ -1,10 +1,8 @@
 class Solution:
     def pathInZigZagTree(self, label: int) -> List[int]:
         depth = ceil(math.log2(label+1))
-        if not depth & 1:
-            lb, ub = 1 << (depth-1), (1 << depth)-1
-            diff = label-lb
-            label = ub-diff
+        if not depth & 1: 
+            label = (1 << (depth-1)) + (1 << depth)-1 - label
 
         res = []
 
@@ -16,7 +14,6 @@ class Solution:
 
         for i in range(1, len(res), 2):
             lb, ub = 1 << i, (1 << (i+1))-1
-            diff = res[i]-lb
-            res[i] = ub-diff
+            res[i] = ub + lb - res[i]
 
         return res
