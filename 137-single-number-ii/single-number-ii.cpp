3666f721f55816126a43e6621290e121f146ec8e
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-03-13 18:29:46 +0530

    Time: 5 ms (67.17%) | Memory: 11.7 MB (76.59%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int arr[33];
+        memset(arr, 0, sizeof(arr));
+
+        for (int num: nums){
+            for (int pos = 0; pos <= 31; pos++){
+                arr[pos] = (arr[pos]+(num&1))%3;
+                num = num >> 1;
+            }
+        }
+
+        int res = 0;
+        for (int i = 32; i >= 0; i--){
+            res = (res<<1) | (arr[i]);
+        }
+        return res;
+
+    }
+};
