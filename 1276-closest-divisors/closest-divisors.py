33375522729cb215cc6141ee725b301797013574
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-03-19 13:28:01 +0530

    Time: 77 ms (97.67%) | Memory: 16.6 MB (68.22%) - LeetSync

--- 1276-closest-divisors/closest-divisors.py
+++ 1276-closest-divisors/closest-divisors.py
@@ -1,8 +1,6 @@
 class Solution:
     def closestDivisors(self, num: int) -> List[int]:
-        def findClosest(n):
-            for i in range(int(n**0.5), 0, -1):
-                if not n%i: return [i, n//i]
-        
-        a, b = findClosest(num+1), findClosest(num+2)
-        return min(a, b, key=lambda x: abs(x[0]-x[1]))
+        a, b = num + 1, num + 2
+        for i in range(int(max(a, b)**0.5), 0, -1):
+            if not a%i: return [i, a//i]
+            if not b%i: return [i, b//i]
