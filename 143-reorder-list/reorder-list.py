cc61aebca5c5c87f8e1c54f9c077e673b665ac18
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-03-23 12:10:56 +0530

    Time: 49 ms (87.54%) | Memory: 24.6 MB (50.88%) - LeetSync

---
+++ 143-reorder-list/reorder-list.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        dq = deque()
+        while head:
+            dq.append(head)
+            head = head.next
+
+        curr = dq.popleft()
+        for i in range(len(dq)):
+            if i & 1: curr.next = dq.popleft()
+            else: curr.next = dq.pop()
+            curr = curr.next
+        curr.next = None
