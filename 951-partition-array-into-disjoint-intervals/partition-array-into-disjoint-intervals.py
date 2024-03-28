ae8f18b6cd15cedf03439c9f33316b40a3e98e20
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-03-28 12:46:35 +0530

    Time: 811 ms (47.78%) | Memory: 30.8 MB (17.75%) - LeetSync

--- 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.py
+++ 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.py
@@ -1,9 +1,8 @@
 class Solution:
     def partitionDisjoint(self, nums: List[int]) -> int:
-        maxel = list(nums)
-        for i in range(1, len(nums)): maxel[i] = max(maxel[i], maxel[i-1])
-        minel = list(nums)
+        maxel, minel = nums[0], list(nums)
         for i in range(len(nums)-2, -1, -1): minel[i] = min(minel[i], minel[i+1])
         for i in range(1, len(nums)):
-            if maxel[i-1] <= minel[i]: return i
+            if maxel <= minel[i]: return i
+            maxel = max(maxel, nums[i])
         return len(nums)
