70759331a08cf31b09cc785f1b7918c5199e8b5d
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-03-30 12:09:00 +0530

    Time: 418 ms (18.11%) | Memory: 20.6 MB (5.33%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -5,10 +5,7 @@ class Solution:
         ctr = Counter()
         maxmap, maxhp = {}, []
 
-        vis = set()
-
         for r in range(len(nums)):
-            if nums[r] in maxmap: vis.add(maxmap[nums[r]])
             maxmap[nums[r]] = r
             ctr[nums[r]] += 1
             heappush(maxhp, r)
@@ -16,11 +13,10 @@ class Solution:
             while len(maxmap) > k: 
                 ctr[nums[l]]-=1
                 if ctr[nums[l]] == 0: 
-                    vis.add(maxmap[nums[l]])
                     del maxmap[nums[l]]
                 l += 1
 
-            while maxhp[0] in vis: heappop(maxhp)
+            while (nums[maxhp[0]] not in maxmap) or maxmap[nums[maxhp[0]]] > maxhp[0] : heappop(maxhp)
             if len(maxmap) < k: continue
 
             tot += maxhp[0]-l+1
