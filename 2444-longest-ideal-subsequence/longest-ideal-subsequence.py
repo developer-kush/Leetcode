961968a7ee76cacfae1595b1c646d8008ca8b8de
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-04-25 11:40:26 +0530

    Time: 351 ms (90.37%) | Memory: 17.5 MB (21.48%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
@@ -1,16 +1,8 @@
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
-
-        def idx(char): return ord(char)-ord('a')
-
-        dp = [[0]*26 for _ in range(len(s)+1)]
-
-        for pos in range(1, len(s)+1):
-            for last in range(26):
-                if abs(last - idx(s[pos-1])) > k: dp[pos][last] = dp[pos-1][last]
-                else: 
-                    take = dp[pos-1][idx(s[pos-1])]+1
-                    notake = dp[pos-1][last]
-                    dp[pos][last] = max(take, notake)
-    
-        return max(dp[-1])
+        
+        lngths = [0]*26
+        for ch in s:
+            i = ord(ch)-97
+            lngths[i] = max(lngths[max(0, i-k): min(26, i+k+1)]) + 1
+        return max(lngths)
