566a3822fadc64ce90a0a3b69fe4b8fbaa0f0c7d
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-05-03 18:27:50 +0530

    Time: 2408 ms (11.58%) | Memory: 78.8 MB (25.87%) - LeetSync

--- 2818-maximum-strictly-increasing-cells-in-a-matrix/maximum-strictly-increasing-cells-in-a-matrix.py
+++ 2818-maximum-strictly-increasing-cells-in-a-matrix/maximum-strictly-increasing-cells-in-a-matrix.py
@@ -8,14 +8,16 @@ class Solution:
             for j in range(m):
                 valij[mat[i][j]].append((i, j))
 
+        res = 1
+
         for key in sorted(valij, reverse = True):
             row_c, col_c = defaultdict(int), defaultdict(int)
             for i, j in valij[key]:
                 potential = max(rowmax[i], colmax[j]) + 1
-                mat[i][j] = potential
                 row_c[i] = max(row_c[i], potential)
                 col_c[j] = max(col_c[j], potential)
+                res = max(res, row_c[i], col_c[i])
             for key in row_c: rowmax[key] = max(rowmax[key], row_c[key])
             for key in col_c: colmax[key] = max(colmax[key], col_c[key])
 
-        return max(max(rowmax.values()), max(colmax.values()))
+        return res
