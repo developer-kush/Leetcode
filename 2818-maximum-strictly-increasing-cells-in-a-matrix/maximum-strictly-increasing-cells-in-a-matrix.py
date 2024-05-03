5c9fbaf0ad3c9d0714edcb08b2290fb543513c0f
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-05-03 18:25:09 +0530

    Time: 2219 ms (16.99%) | Memory: 69.8 MB (79.92%) - LeetSync

---
+++ 2818-maximum-strictly-increasing-cells-in-a-matrix/maximum-strictly-increasing-cells-in-a-matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxIncreasingCells(self, mat: List[List[int]]) -> int:    
+        n, m = len(mat), len(mat[0])
+        colmax, rowmax = defaultdict(int), defaultdict(int)
+
+        valij = defaultdict(list)
+        for i in range(n):
+            for j in range(m):
+                valij[mat[i][j]].append((i, j))
+
+        for key in sorted(valij):
+            row_c, col_c = defaultdict(int), defaultdict(int)
+            for i, j in valij[key]:
+                potential = max(rowmax[i], colmax[j]) + 1
+                mat[i][j] = potential
+                row_c[i] = max(row_c[i], potential)
+                col_c[j] = max(col_c[j], potential)
+            for key in row_c: rowmax[key] = max(rowmax[key], row_c[key])
+            for key in col_c: colmax[key] = max(colmax[key], col_c[key])
+
+        return max(max(rowmax.values()), max(colmax.values()))
