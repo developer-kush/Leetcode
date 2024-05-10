257ba6fb5c13ca7055a46282927c9c48e1cf6944
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-05-10 22:24:34 +0530

    Time: 2264 ms (16.95%) | Memory: 153.8 MB (5.08%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,8 +1,5 @@
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        fractions = []
-        for i in range(len(arr)):
-            for j in range(len(arr)):
-                fractions.append([arr[i], arr[j]])
+        fractions = [[arr[i], arr[j]] for i in range(len(arr)) for j in range(len(arr))]
         fractions = sorted(fractions, key = lambda x: x[0]/x[1])
         return fractions[k-1]
