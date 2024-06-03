8cb5fd3c98c79428063829391e36b8051f75f435
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-06-03 12:04:13 +0530

    Time: 594 ms (57.57%) | Memory: 37.3 MB (25.53%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,11 +1,10 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        rev = s[::-1]
-
+        
         @lru_cache(6500)
-        def lps(x, y):
-            if x < 0 or y < 0: return 0
-            if s[x] == rev[y]: return lps(x-1, y - 1) + 1
-            else: return max(lps(x-1, y), lps(x, y-1))
-
-        return len(s) - lps(len(s)-1, len(s)-1)
+        def rec(x, y):
+            if y <= x: return 0
+            if s[x] == s[y]: return rec(x+1, y-1)
+            return min(rec(x+1, y), rec(x, y-1))+1
+        
+        return rec(0, len(s)-1)
