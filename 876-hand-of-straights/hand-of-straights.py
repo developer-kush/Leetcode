ea47f5c2de9da2730f0e6f36c87d62f1349d284e
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-06-06 08:56:17 +0530

    Time: 189 ms (24.12%) | Memory: 18.6 MB (28.91%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -7,17 +7,13 @@ class Solution:
         curr = {}
 
         for val in hand:
-            if val - 1 in curr:
-                last = curr[val-1].pop()
-                if not curr[val-1]: del curr[val-1]
-                
-                last += 1
+            if val - 1 in curr and curr[val-1]:
+                last = curr[val-1].pop() + 1
                 if last != groupSize:
                     if val not in curr: curr[val] = []
                     curr[val].append(last)
-                
             else: 
                 if val not in curr: curr[val] = []
                 curr[val].append(1)
                 
-        return not curr
+        return not (curr and any(curr.values()))
