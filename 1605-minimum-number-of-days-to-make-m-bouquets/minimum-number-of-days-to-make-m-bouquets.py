ba83ee406abacf8f3d77a20b868632155d7806f2
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-06-19 11:55:34 +0530

    Time: 1034 ms (15.13%) | Memory: 40 MB (14.71%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        if len(bloomDay) < m*k: return -1
+        n = len(bloomDay)
+        
+        hp = []
+        dp = [0]*n
+        for i in range(n-1, -1, -1):
+            heappush(hp, (-bloomDay[i], i))
+            while hp[0][1] >= i + k: heappop(hp)
+            dp[i] = -hp[0][0]
+        
+        def check(x, dp, n, k, m):
+            ptr = cnt = 0
+            while ptr < n:
+                if ptr + k > len(bloomDay): break
+                if dp[ptr] <= x: 
+                    ptr = ptr + k
+                    cnt += 1
+                else: ptr += 1
+                if cnt >= m: return True
+            return cnt >= m
+        
+        l, r = min(bloomDay), max(bloomDay)
+        while l < r:
+            mid = (l + r) >> 1
+            possible = check(mid, dp, n, k, m)
+            if possible: r = mid
+            else: l = mid+1
+        return l
