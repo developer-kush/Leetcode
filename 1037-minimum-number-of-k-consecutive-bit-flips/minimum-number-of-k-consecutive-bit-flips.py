e535a48ac814a1f5e6c987e1eb27033e8a6f4399
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-06-24 08:53:43 +0530

    Time: 869 ms (13.38%) | Memory: 21.2 MB (26.76%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.py
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.py
@@ -2,20 +2,16 @@ class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
         q = deque()
         cnt = 0
-        for idx in range(len(nums)-k+1):
+        bp = len(nums)-k+1
+
+        for idx in range(len(nums)):
             while q and idx - q[0] >= k: q.popleft()
 
             if len(q)&1: num = 1^nums[idx]
             else: num = nums[idx]
-            
-            if num == 0: cnt += 1; q.append(idx)
+
+            if idx >= bp:
+                if num == 0: return -1
+            elif num == 0: cnt += 1; q.append(idx)
         
-        for idx in range(len(nums)-k+1, len(nums)):
-            while q and idx - q[0] >= k: q.popleft()
-
-            if len(q)&1: num = 1^nums[idx]
-            else: num = nums[idx]
-
-            if num == 0: return -1
-
         return cnt
