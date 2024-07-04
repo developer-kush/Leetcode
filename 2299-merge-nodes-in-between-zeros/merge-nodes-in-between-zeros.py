f6e72725ce21c5154f7c4eb2a1d39a01d101c519
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-07-04 09:01:26 +0530

    Time: 955 ms (54.77%) | Memory: 75.2 MB (20.73%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.py
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.py
@@ -10,18 +10,11 @@ class Solution:
         head = head.next
         
         while head:
-            if head.val == 0:  
+            if head.val == 0 and head.next:  
                 curr.next = ListNode()
                 curr = curr.next
             else:
                 curr.val += head.val
             head = head.next
-        
-        curr = res
-        while curr:
-            if curr.next and curr.next.val == 0:
-                curr.next = None
-                break
-            curr = curr.next
         
         return res
