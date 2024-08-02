1d0d286dc70b2774fcf7341e85ee572c52c729e1
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-08-02 16:55:51 +0530

    Time: 750 ms (36.94%) | Memory: 21.2 MB (5.63%) - LeetSync

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        zc = sum(1-num for num in nums)
+
+        nums += nums
+
+        cc, res = 0, inf
+
+        for i in range(zc): cc += 1-nums[i]
+        res = min(res, zc-cc)
+
+        for i in range(zc, len(nums)):
+            cc += 1-nums[i]
+            cc -= 1-nums[i-zc]
+            res = min(res, zc-cc)
+        
+        return res
