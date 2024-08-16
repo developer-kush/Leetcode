bed824c645f6167338e5644ebe50056d56bc9b3a
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-08-16 09:13:52 +0530

    Time: 1468 ms (9.77%) | Memory: 42.5 MB (63.79%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,13 +1,17 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
         taken, mx, mn = set(), [], []
+        for val in arrays[0]: 
+            if val not in taken:
+                taken.add(val)
+                heappush(mn, val)
+                heappush(mx, -val)
 
         res = 0
-        for row in arrays:
+        for i in range(1, len(arrays)):
+            row = arrays[i]
 
-            for val in row:
-                if not mn: continue
-                res = max(res, abs(val-mn[0]), abs(val+mx[0]))
+            for val in row: res = max(res, abs(val-mn[0]), abs(val+mx[0]))
 
             for val in row: 
                 if val not in taken:
