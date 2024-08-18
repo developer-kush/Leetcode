10483dbfdc2d6479609a21d5eefa99649c5eb314
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-08-18 21:51:47 +0530

    Time: 42 ms (98.15%) | Memory: 16.7 MB (18.38%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -1,13 +1,17 @@
+scores = []
+
+hp = [[1, 0, 0, 0]]
+at = { 1 }
+for i in range(1690):
+    num, f2, f3, f5 = heappop(hp)
+    scores.append(num)
+    
+    for a, b, c, d in [(num*2, f2+1, f3, f5), (num*3, f2, f3+1, f5), (num*5, f2, f3, f5+1)]:
+        if a in at: continue
+        heappush(hp, [a, b, c, d])
+        at.add(a)
+
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        hp = [[1, 0, 0, 0]]
-        at = { 1 }
-        for i in range(n-1):
-            num, f2, f3, f5 = heappop(hp)
-            
-            for a, b, c, d in [(num*2, f2+1, f3, f5), (num*3, f2, f3+1, f5), (num*5, f2, f3, f5+1)]:
-                if a in at: continue
-                heappush(hp, [a, b, c, d])
-                at.add(a)
-        
-        return heappop(hp)[0]
+        global scores
+        return scores[n-1]
