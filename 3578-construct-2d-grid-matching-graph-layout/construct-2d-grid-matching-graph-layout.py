8e39010066014da353957822e9b7fd99e1e8ffee
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-10-07 10:26:27 +0530

    Time: 2616 ms (100.00%) | Memory: 65.7 MB (100.00%) - LeetSync

--- 3578-construct-2d-grid-matching-graph-layout/construct-2d-grid-matching-graph-layout.py
+++ 3578-construct-2d-grid-matching-graph-layout/construct-2d-grid-matching-graph-layout.py
@@ -18,27 +18,18 @@ class Solution:
             return res
         
         deg = {i:len(graph[i]) for i in range(n)}
-        
-        if 1 in deg.values():
-            st = [i for i in range(n) if deg[i]==1][0]
-            bfs = getBFS(st)
-            res = [0]*n
-            for i in range(n): res[bfs[i]]=i
-            return [res]
 
-        starts = [i for i in range(n) if deg[i]==2]
+        mndeg = min(deg.values())
+        starts = [i for i in range(n) if deg[i]==mndeg]
         s1 = starts[0]
         bfs1 = getBFS(s1)
         maxd = max(bfs1.values())
-        s2 = [i for i in starts if bfs1[i]!=0 and bfs1[i]!=maxd][0]
+        s2 = sorted(starts, key=lambda x: bfs1[x])[1]
         bfs2 = getBFS(s2)
         
         m, n = (bfs1[s2]+1, maxd-bfs1[s2]+1)
         res = [[0]*m for _ in range(n)]
-
-        # print(bfs1[s2], maxd)
-        # print(n, m, ":", s1, s2)
-
+        
         for i in range(len(graph)):
             a, b = bfs1[i], bfs2[i]
             b -= (m-1)
