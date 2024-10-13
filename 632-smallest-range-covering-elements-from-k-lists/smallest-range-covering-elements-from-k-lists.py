d91dfc30eaed02a077d3d8457523471fd90e5277
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-10-13 14:34:10 +0530

    Time: 195 ms (52.72%) | Memory: 24 MB (20.05%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
@@ -0,0 +1,28 @@
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        numGrps = defaultdict(Counter)
+        for idx, arr in enumerate(nums): 
+            for val in arr: numGrps[val][idx] += 1
+
+        srtKeys = sorted(numGrps)
+        covcnt = Counter()
+
+        l, mn = 0, float('inf')
+        res = []
+
+        def possible(l):
+            return all(covcnt[grp] > val for  grp, val in numGrps[srtKeys[l]].items())
+
+        for r in range(len(srtKeys)):
+            for grp, val in numGrps[srtKeys[r]].items(): covcnt[grp] += val
+            
+            while l < r and possible(l):
+                for grp, val in numGrps[srtKeys[l]].items(): 
+                    covcnt[grp] -= val
+                l += 1
+            
+            if len(covcnt) == len(nums) and srtKeys[r]-srtKeys[l]+1 < mn:
+                mn = srtKeys[r]-srtKeys[l]+1
+                res = [srtKeys[l], srtKeys[r]]
+
+        return res
