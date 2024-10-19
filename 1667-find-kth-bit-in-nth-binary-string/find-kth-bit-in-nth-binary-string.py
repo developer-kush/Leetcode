efdab97a8be277c188de1ba6b6ca072f1f6583c7
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-10-19 08:46:02 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (72.78%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -1,21 +1,24 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
 
-        ln = (1<<n) - 1
+        ln = (1 << n) - 1
 
         def rec(k, inv, ln):
 
             if ln == 1:
-                if inv&1: return '1'
-                return '0'
+                if inv & 1:
+                    return "1"
+                return "0"
 
-            nln = ln>>1
+            nln = ln >> 1
             if k == nln:
-                if inv&1: return'0'
-                return '1'
-            
-            if k > nln: return rec(nln+nln-k, inv+1, nln)
-            
+                if inv & 1:
+                    return "0"
+                return "1"
+
+            if k > nln:
+                return rec(nln + nln - k, inv + 1, nln)
+
             return rec(k, inv, nln)
 
-        return rec(k-1, 0, ln)
+        return rec(k - 1, 0, ln)
