bb63a20d0a5866ae2ebba1a2cdaa284106918c57
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-10-30 09:52:16 +0530

    Time: 19 ms (86.05%) | Memory: 16.9 MB (31.03%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -7,21 +7,17 @@ class Solution:
         
         for i in range(n-1, -1, -1):
             if not lis_b or nums[i] > lis_b[-1]: lis_b.append(nums[i])
-            else: 
-                pos = bisect_left(lis_b, nums[i])
-                lis_b[pos] = nums[i]
+            else: lis_b[bisect_left(lis_b, nums[i])] = nums[i]
             lis[i] = len(lis_b)
         
         res = inf
         lis_f = []
+
         for i in range(n):
             if not lis_f or nums[i] > lis_f[-1]: lis_f.append(nums[i])
-            else: 
-                pos = bisect_left(lis_f, nums[i])
-                lis_f[pos] = nums[i]
+            else: lis_f[bisect_left(lis_f, nums[i])] = nums[i]
 
             if len(lis_f)==1 or lis[i]==1: continue
-
             moun = len(lis_f) + lis[i] - 1
             res = min(res, n-moun)
             
