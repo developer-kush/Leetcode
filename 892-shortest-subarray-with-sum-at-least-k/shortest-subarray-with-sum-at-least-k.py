f8af65a47e62b655dedaf172624644ea90cbbe02
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2024-11-17 16:17:02 +0530

    Time: 405 ms (14.53%) | Memory: 39.9 MB (5.22%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -4,8 +4,6 @@ class Solution:
         bsarr = [[0, -1]]
         for i in range(1, n): nums[i] += nums[i-1]
 
-        print(nums)
-
         res = inf
         
         for r in range(n):
@@ -16,8 +14,6 @@ class Solution:
             if tgt < bsarr[0][0] : continue
             
             pos = bisect_left(bsarr, [tgt+1, -1])-1
-            # print(f"{bsarr = }")
-            # print(f"tgt: {tgt} pos: {pos} | r: {r} tgtval: {bsarr[pos][1]} curr: {r-bsarr[pos][1]}")
             res = min(res, r-bsarr[pos][1])
 
         return -1 if res is inf else res
