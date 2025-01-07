1868aa5cfe7a375f5ade582e4d0d0f75816bdb3a
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2025-01-07 09:02:45 +0530

    Time: 4 ms (28.54%) | Memory: 17.9 MB (12.59%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,3 +1,6 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        return [word for idx, word in enumerate(words) if any(word in x for i,x in enumerate(words) if i!=idx)]
+        return [
+            word for word in words
+            if any(word in x for x in words if x!=word)
+        ]
