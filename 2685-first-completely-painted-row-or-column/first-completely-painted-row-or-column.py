ff530d221dcfc52cd9d90c58b3c767f047503745
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2025-01-20 11:34:54 +0530

    Time: 111 ms (79.49%) | Memory: 51.9 MB (53.31%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        r, c = len(mat), len(mat[0])
+        nummap = {}
+        for i in range(r):
+            for j in range(c):
+                nummap[mat[i][j]] = (i, j)
+        rows = [c]*r
+        cols = [r]*c
+        for idx, i in enumerate(arr):
+            x, y  = nummap[i]
+            rows[x] -= 1
+            cols[y] -= 1
+            if rows[x] == 0 or cols[y] == 0:
+                return idx
