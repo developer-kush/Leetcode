93f75aa39ab2e643318894a55bb14f1525cf7511
Kushagra Agarwal <94402194+developer-kush@users.noreply.github.com>
2025-01-21 21:13:10 +0530

    Time: 137 ms (26.89%) | Memory: 29.5 MB (72.79%) - LeetSync

--- 2145-grid-game/grid-game.py
+++ 2145-grid-game/grid-game.py
@@ -2,20 +2,13 @@ class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
 
         n = len(grid[0])
-
-        def getRangeSum(arr):
-            cpy = list(arr)
-            for i in range(1, len(cpy)): cpy[i]+=cpy[i-1]
-            def inner(l, r):
-                if l == 0: return cpy[r]
-                else: return cpy[r] - cpy[l-1]
-            return inner
         
         a, b = grid
-        geta, getb = getRangeSum(a), getRangeSum(b)
+        for i in range(1, n):
+            a[i] += a[i-1]
+            b[i] += b[i-1]
 
-        res = min(geta(1, n-1), getb(0, n-2))
-        for i in range(1, n-1):
-            res = min(res, max(getb(0, i-1), geta(i+1, n-1)))
+        res = min(a[n-1]-a[0], b[n-2])
+        for i in range(1, n-1): res = min(res, max(b[i-1], a[n-1]-a[i]))
 
         return res
